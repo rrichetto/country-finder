22af0d20a409bc1760c436c3a6607531cdfb8449
Ryan <rrichetto@comcast.net>
2019-06-20 01:11:55 -0500

    adds filter capabilities

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,8 @@ import UI from './modules/ui.js';
 
 // Cache the DOM
 const searchEl = document.querySelector('.nav__search');
+const toggleEl = document.querySelector('.nav__toggle');
+const optionsEl = document.querySelector('.nav__region-options');
 
 
 // Instatiate countries
@@ -12,25 +14,40 @@ const ui = new UI();
 // Initialize main page with all countries
 showCountries();
 
-// Search input event listener
-searchEl.addEventListener('keyup', showCountries)
+let currentRegion = 'all';
 
-
+// Listeners
+searchEl.addEventListener('keyup', showCountries);
+toggleEl.addEventListener('click', ui.toggleFilter);
+optionsEl.addEventListener('click', (e) => {
+  ui.setActiveRegion(e);
+  currentRegion = ui.getActiveRegion();
+  showCountries(currentRegion);
+  optionsEl.style.display = 'none';
+})
 
 
 // Initialize main page with all countries
-function showCountries(e) {
+function showCountries() {
+
   countries.getAllCountries()
+    .then(data => {
+      if (currentRegion === 'all') {
+        return data;
+      } else {
+        return filter(data, currentRegion);
+      }
+    })
     .then(data => {
       if (searchEl.value === '') {
         ui.renderCountries(data);
       } else {
-        ui.renderCountries(searchCountries(data));
+        ui.renderCountries(search(data));
       }
     })
 }
 
-function searchCountries(countries) {
+function search(countries) {
   const input = searchEl.value.toLowerCase();
   const filtered = [];
 
@@ -43,3 +60,14 @@ function searchCountries(countries) {
   return filtered;
 }
 
+function filter(countries, region) {
+  const filtered = [];
+
+  countries.forEach(country => {
+    if (country.region.toLowerCase() === region.toLowerCase()) {
+      filtered.push(country);
+    }
+  });
+
+  return filtered;
+}
--- dist/js/modules/ui.js
+++ dist/js/modules/ui.js
@@ -1,4 +1,9 @@
+const optionsEl = document.querySelector('.nav__region-options');
+const optionEls = document.querySelectorAll('.nav__region');
+const regionNameEl = document.querySelector('.nav__region-name');
+
 export default class UI {
+
   renderCountries(countries) {
     let markup = '';
 
@@ -17,6 +22,35 @@ export default class UI {
     })
 
     document.querySelector('.countries').innerHTML = markup;
+  }
+
+  getActiveRegion() {
+    let region = '';
+
+    optionEls.forEach(option => {
+      if (option.classList.contains('active')) {
+        region = option.dataset.region;
+      }
+    })
+
+    return region;
+  }
+
+  setActiveRegion(e) {
+    optionEls.forEach(option => {
+      option.classList.remove('active');
+    })
+
+    e.target.classList.add('active');
+    regionNameEl.textContent = e.target.textContent;
+  }
+
+  toggleFilter() {
+    if (optionsEl.style.display === 'flex') {
+      optionsEl.style.display = 'none';
+    } else {
+      optionsEl.style.display = 'flex';
+    }
   }
 }
 
