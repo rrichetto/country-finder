097c3ad0fcdb1bf45b3474480469c950f75575ed
Ryan <rrichetto@comcast.net>
2019-06-20 04:38:56 -0500

    adds Details for showing country details

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,17 +6,24 @@ const searchEl = document.querySelector('.nav__search');
 const toggleEl = document.querySelector('.nav__toggle');
 const optionsEl = document.querySelector('.nav__region-options');
 
-
 // Instatiate countries
 const countries = new Countries();
 // Instantiate UI
 const ui = new UI();
 // Initialize main page with all countries
-showCountries();
+// showCountries();
 
 let currentRegion = 'all';
 
 // Listeners
+window.addEventListener('DOMContentLoaded', () => {
+  if (location.href.includes('index')) {
+    showCountries();
+  } else {
+    const code = location.hash.split('#')[1];
+    showDetails(code)
+  }
+})
 searchEl.addEventListener('keyup', showCountries);
 toggleEl.addEventListener('click', ui.toggleFilter);
 optionsEl.addEventListener('click', (e) => {
@@ -44,6 +51,18 @@ function showCountries() {
       } else {
         ui.renderCountries(search(data));
       }
+    })
+}
+
+function showDetails(code) {
+  countries.getAllCountries()
+    .then(data => {
+      return data.find(country => {
+        return country.numericCode === code;
+      })
+    })
+    .then(data => {
+      ui.renderDetails(data);
     })
 }
 
--- dist/js/modules/ui.js
+++ dist/js/modules/ui.js
@@ -9,19 +9,42 @@ export default class UI {
 
     countries.forEach(country => {
       markup += `
-        <div class="card">
+        <a href="details.html#${country.numericCode}" class="card">
           <img class="card__flag" src=${country.flag}>
           <div class="card__content">
             <h3 class="card__name">${country.name}</h3>
-            <p class="card__population"><span>Population:</span> ${country.population}</p>
+            <p class="card__population"><span>Population:</span> ${formatNum(country.population)}</p>
             <p class="card__region"><span>Region:</span> ${country.region}</p>
             <p class="card__capital"><span>Capital:</span> ${country.capital}</p>
           </div>
-        </div>
+        </a>
       `;
     })
 
     document.querySelector('.countries').innerHTML = markup;
+  }
+
+  renderDetails(country) {
+    let markup = `
+      <div class="details__img-box">
+        <img src="${country.flag}" alt="" class="details__flag">
+      </div>
+      <div class="details__content">
+        <h2 class="details__name">${country.name}</h2>
+        <div class="details__info-box">
+          <p><span>Native Name: </span>${country.nativeName}</p>
+          <p><span>Population: </span>${formatNum(country.population)}</p>
+          <p><span>Region: </span>${country.region}</p>
+          <p><span>Sub Region: </span>${country.subregion}</p>
+          <p><span>Capital: </span>${country.capital}</p>
+          <p><span>Top Level Domain: </span>${country.topLevelDomain}</p>
+          <p><span>Currency: </span>${country.currencies[0].name}</p>
+          <p><span>Language: </span>${country.languages[0].name}</p>
+        </div>
+      </div>
+    `;
+
+    document.querySelector('.details').innerHTML = markup;
   }
 
   getActiveRegion() {
@@ -54,6 +77,9 @@ export default class UI {
   }
 }
 
+function formatNum(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 
 
